b9eaef27df09e8ea3f115be575d5066ff8422672
Mark Edelman <markedelman1987@gmail.com>
2016-11-22 11:15:02 -0500

    demo for branching

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log("Hello console")
+
+// print hello world 10 times
+
+function sayHello(n) {
+    for (var i = 0; i < n; i++) {
+        console.log('Hello world! ' + (i + 1) + ' time');
+    }
+}
+
+// call the function
+sayHello(10);
