d4dd7ad17649f6bd2e1dcd853115294c389a8d59
Mark Edelman <markedelman1987@gmail.com>
2016-11-22 12:06:46 -0500

    JS Mentor Session Hello_World

--- app.js
+++ app.js
@@ -1,1 +1,64 @@
 console.log("Hello console")
+
+/*
+- Assign name 'Mark' to variable called `name`
+- console log, 'Hello Mark'
+*/
+
+// var name = "Mark";
+// console.log("Hello" + " " + name);
+
+// name = "Anthony";
+// console.log("Hello " + name);
+
+// name = "Jeb";
+// console.log("Hello " + name);
+
+/*
+function <funtionName>(<arguments>) {
+	<function body>
+	return <results>
+}
+
+
+(inputs) --> [function] --> (output)
+*/
+
+function greeter(name) {
+	var greeting = "Alo " + name;
+	return greeting;
+}
+
+// function greeter(name) {
+// 	return "Hello " + name;
+// }
+
+var name = "Mark";
+console.log(greeter(name));
+console.log(greeter("Anthony"));
+console.log(greeter("Jane"));
+console.log(greeter("John"));
+console.log(greeter("Jack"));
+console.log(greeter("James"));
+
+// JS datatypes
+
+var n = "Mark";
+var x = 20;
+var isStudent = false;
+var start = null;
+var student = {
+	name: 'Mark',
+	course: 'Prep Course Web',
+	school: 'Thinkful',
+};
+var j;
+
+// main JS datatypes
+console.log('n: ', n, typeof n);
+console.log('x: ', x, typeof x);
+console.log('isStudent: ', isStudent, typeof isStudent);
+console.log('start: ', start, typeof start);
+console.log('j: ', j, typeof j);
+console.log('student: ', typeof student);
+
